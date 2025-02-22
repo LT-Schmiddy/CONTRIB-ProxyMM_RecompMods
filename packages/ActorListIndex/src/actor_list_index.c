a171f4abaf76dccd9a881eb928abf53f3d9bccb4
Garrett Cox <garrettjcox@gmail.com>
2025-02-22 12:50:10 -0600

    Fix issue with ActorListIndex not bein available in should_actor_init

--- packages/ActorListIndex/src/actor_list_index.c
+++ packages/ActorListIndex/src/actor_list_index.c
@@ -6,6 +6,7 @@ void Actor_KillAllOnHalfDayChange(PlayState* play, ActorContext* actorCtx);
 Actor* Actor_SpawnEntry(ActorContext* actorCtx, ActorEntry* actorEntry, PlayState* play);
 
 ActorExtensionId sActorListIndexExtensionId;
+s32 currentActorListIndex = 0;
 
 RECOMP_CALLBACK("*", recomp_on_init) void OnRecompInit() {
     sActorListIndexExtensionId = z64recomp_extend_actor_all(sizeof(s32));
@@ -35,6 +36,10 @@ RECOMP_PATCH void Actor_SpawnSetupActors(PlayState* play, ActorContext* actorCtx
         shiftedHalfDaysBit = (actorCtx->halfDaysBit << 1) & (HALFDAYBIT_ALL & ~HALFDAYBIT_DAY0_NIGHT);
 
         for (i = 0; i < play->numSetupActors; i++) {
+            // #region @ProxyMM store the actor's index + 1 to be used for identification
+            currentActorListIndex = i + 1;
+            // #endregion
+
             actorEntryHalfDayBit = ((actorEntry->rot.x & 7) << 7) | (actorEntry->rot.z & 0x7F);
             if (actorEntryHalfDayBit == 0) {
                 actorEntryHalfDayBit = HALFDAYBIT_ALL;
@@ -43,16 +48,31 @@ RECOMP_PATCH void Actor_SpawnSetupActors(PlayState* play, ActorContext* actorCtx
             if (!(actorEntryHalfDayBit & prevHalfDaysBitValue) && (actorEntryHalfDayBit & actorCtx->halfDaysBit) &&
                 (!CHECK_EVENTINF(EVENTINF_17) || !(actorEntryHalfDayBit & shiftedHalfDaysBit) ||
                  !(actorEntry->id & 0x800))) {
-                // #region @ProxyMM store the actor's index + 1, reasoning @ GetActorListIndex
-                Actor* actor = Actor_SpawnEntry(&play->actorCtx, actorEntry, play);
-                s32* actorListIndex = z64recomp_get_extended_actor_data(actor, sActorListIndexExtensionId);
-                *actorListIndex = i + 1;
-                // #endregion
+                Actor_SpawnEntry(&play->actorCtx, actorEntry, play);
             }
             actorEntry++;
         }
+        // #region @ProxyMM Reset the current index
+        currentActorListIndex = 0;
+        // #endregion
 
         // Prevents re-spawning the setup actors
         play->numSetupActors = -play->numSetupActors;
     }
+}
+
+// @ProxyMM This is a bit of a hack, but we need to store the ActorListIndex prior to `should_actor_init` being called,
+// but after the ActorExtension memory is made available. Unfortunately these both happen in `Actor_Init` which base
+// recomp patches. We are lucky though, because in between these events, the following function is called, and never
+// used again in the codebase. So we'll hook in here to store our ActorListIndex
+RECOMP_PATCH void Actor_SetWorldToHome(Actor* actor) {
+    actor->world = actor->home;
+
+    // #region @ProxyMM
+    if (currentActorListIndex != 0) {
+        s32* actorListIndex = z64recomp_get_extended_actor_data(actor, sActorListIndexExtensionId);
+        *actorListIndex = currentActorListIndex;
+        currentActorListIndex = 0;
+    }
+    // #endregion
 }
