6a6330e6cb19d42b6628a4097f2da98321fb12ba
LT-Schmiddy <schmidalex1996@gmail.com>
2025-05-17 16:55:37 -0400

    saves kv_store with save file.

--- packages/KV/src/main.c
+++ packages/KV/src/main.c
@@ -1,17 +1,26 @@
 #include "recomp_api.h"
 #include "z64extern.h"
 
+RECOMP_IMPORT(".", bool KV_PathUpdateInternal(unsigned char* key));
 RECOMP_IMPORT(".", bool KV_Get(const char* key, void* dest, u32 size, u8 slot));
 RECOMP_IMPORT(".", bool KV_Set(const char* key, void* data, u32 size, u8 slot));
 RECOMP_IMPORT(".", bool KV_Has(const char* key, u8 slot));
 RECOMP_IMPORT(".", bool KV_Remove(const char* key, u8 slot));
 
+void KV_PathUpdate() {
+    unsigned char* new_path = recomp_get_save_file_path();
+    KV_PathUpdateInternal(new_path);
+    recomp_free(new_path);
+}
+
 RECOMP_EXPORT bool KV_Global_Get(const char* key, void* dest, u32 size) {
+    KV_PathUpdate();
     return KV_Get(key, dest, size, 255);
 }
 
 #define DEFINE_KV_GLOBAL_GETTER(type, suffix) \
 RECOMP_EXPORT type KV_Global_Get_##suffix(const char* key, type defaultValue) { \
+    KV_PathUpdate(); \
     type value = defaultValue; \
     KV_Get(key, &value, sizeof(type), 255); \
     return value; \
@@ -25,11 +34,13 @@ DEFINE_KV_GLOBAL_GETTER(u32, U32)
 DEFINE_KV_GLOBAL_GETTER(s32, S32)
 
 RECOMP_EXPORT bool KV_Global_Set(const char* key, void* data, u32 size) {
+    KV_PathUpdate(); 
     return KV_Set(key, data, size, 255);
 }
 
 #define DEFINE_KV_GLOBAL_SETTER(type, suffix) \
 RECOMP_EXPORT bool KV_Global_Set_##suffix(const char* key, type value) { \
+    KV_PathUpdate(); \
     return KV_Set(key, &value, sizeof(type), 255); \
 }
 
@@ -41,19 +52,23 @@ DEFINE_KV_GLOBAL_SETTER(u32, U32)
 DEFINE_KV_GLOBAL_SETTER(s32, S32)
 
 RECOMP_EXPORT bool KV_Global_Has(const char* key) {
+    KV_PathUpdate(); 
     return KV_Has(key, 255);
 }
 
 RECOMP_EXPORT bool KV_Global_Remove(const char* key) {
+    KV_PathUpdate(); 
     return KV_Remove(key, 255);
 }
 
 RECOMP_EXPORT bool KV_Slot_Get(const char* key, void* data, u32 size) {
+    KV_PathUpdate(); 
     return KV_Get(key, data, size, gSaveContext.fileNum);
 }
 
 #define DEFINE_KV_SLOT_GETTER(type, suffix) \
 RECOMP_EXPORT type KV_Slot_Get_##suffix(const char* key, type defaultValue) { \
+    KV_PathUpdate(); \
     type value = defaultValue; \
     KV_Get(key, &value, sizeof(type), gSaveContext.fileNum); \
     return value; \
@@ -67,11 +82,13 @@ DEFINE_KV_SLOT_GETTER(u32, U32)
 DEFINE_KV_SLOT_GETTER(s32, S32)
 
 RECOMP_EXPORT bool KV_Slot_Set(const char* key, void* data, u32 size) {
+    KV_PathUpdate(); 
     return KV_Set(key, data, size, gSaveContext.fileNum);
 }
 
 #define DEFINE_KV_SLOT_SETTER(type, suffix) \
 RECOMP_EXPORT bool KV_Slot_Set_##suffix(const char* key, type value) { \
+    KV_PathUpdate(); \
     return KV_Set(key, &value, sizeof(type), gSaveContext.fileNum); \
 }
 
@@ -83,9 +100,11 @@ DEFINE_KV_SLOT_SETTER(u32, U32)
 DEFINE_KV_SLOT_SETTER(s32, S32)
 
 RECOMP_EXPORT bool KV_Slot_Has(const char* key) {
+    KV_PathUpdate(); 
     return KV_Has(key, gSaveContext.fileNum);
 }
 
 RECOMP_EXPORT bool KV_Slot_Remove(const char* key) {
+    KV_PathUpdate(); 
     return KV_Remove(key, gSaveContext.fileNum);
 }
